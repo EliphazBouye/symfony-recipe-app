4b0d848759da2c9c3d18b1c1af4ef6292b5fc9fb
Samuel Bouye <bouyesamuel@gmail.com>
2021-11-29 14:56:51 +0000

    CRUD : Read , List

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,12 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RecipeController extends AbstractController
 {
+    public function listRecipe(RecipeRepository $recipeRepository): Response
+    {
+        $recipes = $recipeRepository->findAll();
+
+        return new Response(
+            dump($recipes)
+        );
+    }
+
     public function createRecipe(): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
@@ -22,6 +32,24 @@ class RecipeController extends AbstractController
         $entityManager->flush();
 
         return new Response('Saved recipe with id '.$recipe->getId());
+    }
+
+    public function show(int $id): Response
+    {
+        $recipe = $this->getDoctrine()
+            ->getRepository(Recipe::class)
+            ->find($id);
+
+
+        if(!$recipe) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great recipe: '.$recipe->getContent());
+
+
     }
 
 }
